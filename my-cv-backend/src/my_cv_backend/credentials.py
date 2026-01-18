26f14059a92452fab0644123892b2a5dea9b45e3
RobinNogues <80032361+RobinNogues@users.noreply.github.com>
2026-01-18 12:22:11 +0100

    use caddy instead of nginx, add limiter on form API and simplify .env (#20)
    * use caddy instead of nginx, add limiter on form API and simplify .env
    
    * change caddyfile conf

--- my-cv-backend/src/my_cv_backend/credentials.py
+++ my-cv-backend/src/my_cv_backend/credentials.py
@@ -5,3 +5,4 @@ EMAIL = environ["EMAIL"]
 EMAIL_PASSWORD = environ["EMAIL_PASS"]
 SMTP_SERVER = environ.get("SMTP_SERVER", "smtp.gmail.com")
 SMTP_PORT = int(environ.get("SMTP_PORT", "465"))
+DOMAIN = environ.get("DOMAIN", "localhost")
--- my-cv-backend/src/my_cv_backend/main.py
+++ my-cv-backend/src/my_cv_backend/main.py
@@ -1,28 +1,29 @@
 from fastapi import FastAPI, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
 
-from .credentials import EMAIL
+from .credentials import DOMAIN, EMAIL
 from .email_sender import EMAIL_SENDER, format_content
 from .logger import logger
+from .middleware import RateLimitMiddleware
 from .models import ContactForm
-
 
 app = FastAPI(
     title="CV Contact Form API",
     description="API that send an email to Robin NOGUES when someone uses the contact form",
-    version="1.0.0"
+    version="1.0.0",
 )
 
-from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(RateLimitMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://robin-nogues.com",
-        "https://www.robin-nogues.com",
+        f"https://{DOMAIN}",
+        f"https://www.{DOMAIN}",
         "http://localhost",
         "http://localhost:8080",
         "http://127.0.0.1",
-        "http://127.0.0.1:8080"
+        "http://127.0.0.1:8080",
     ],
     allow_credentials=True,
     allow_methods=["POST"],
@@ -32,19 +33,15 @@ app.add_middleware(
 
 @app.post("/api/contact", status_code=status.HTTP_200_OK)
 async def submit_contact_form(form_data: ContactForm):
-    
     if form_data.address:
         # Return a success response to avoid giving bots clues
-        raise HTTPException(
-            status_code=status.HTTP_200_OK,
-            detail="Invalid request."
-        )
+        raise HTTPException(status_code=status.HTTP_200_OK, detail="Invalid request.")
 
     name = form_data.name.strip()
     email = form_data.email.strip()
     subject = form_data.subject.strip()
     message = form_data.message.strip()
-    
+
     content = format_content(message, name, email)
 
     try:
