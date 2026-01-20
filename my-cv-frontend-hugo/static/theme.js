424855a57fc084f83f966b88df439eebb4bb200b
RobinNogues <robin.nogues@gmail.com>
2026-01-20 13:01:53 +0100

    remove old front

--- my-cv-frontend-hugo/static/theme.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    const theme = localStorage.getItem('theme');
-    const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    if (theme === 'dark' || (!theme && systemPrefersDark)) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-    }
-})();
